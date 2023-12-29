0022d7bff69dbc154e2bf230e98ba681717cef8b
rifky <ee.guy.42@gmail.com>
2023-12-30 04:16:44 +0700

    feat: able to choose answer and display it in results template

--- quiz/views.py
+++ quiz/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,get_object_or_404
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
-from .models import Question
+from django.urls import reverse
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -17,10 +18,28 @@ def detail(request, question_id):
     return render(request, "quiz/detail.html", {"question": question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "quiz/results.html", {"question": question})
 
 def answer(request, question_id):
-    return HttpResponse("You're answering question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(
+            pk=request.POST["choice"]
+        )
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "quiz/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        # selected_choice.save()
+        return render(request, "quiz/results.html", {"question": question, "selected_choice": selected_choice})
+
 
 # Create your views here.
